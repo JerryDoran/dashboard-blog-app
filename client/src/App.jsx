c0c8dc739f7a315c12bd5438e79a31298442c929
Jerome Doran <disneyfan9184@gmail.com>
2024-01-21 13:35:41 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Projects from './pages/projects';
+import Dashboard from './pages/dashboard';
+import About from './pages/about';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'> Mern Blog</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
