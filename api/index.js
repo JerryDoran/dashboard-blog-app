06a6c0930452554140a3c2448b7194b8a94bf874
Jerome Doran <disneyfan9184@gmail.com>
2024-02-01 17:09:06 -0500

    created server

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('route', (req, res) => {});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
