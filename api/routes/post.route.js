4d52b1c96d891eb2dfb8726d3253b08cfc899697
Jerome Doran <disneyfan9184@gmail.com>
2024-03-25 16:13:14 -0400

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../lib/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
+router.get('/get-posts', getPosts);
 
 export default router;
