c7cb753ca0e2aff4ede026b30d5c93b4bb31ab0e
Jerome Doran <disneyfan9184@gmail.com>
2024-04-08 08:59:28 -0400

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUsers,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../lib/verifyUser.js';
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.put('/update/:userid', verifyToken, updateUser);
 router.delete('/delete/:userid', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
